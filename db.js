ac972cac6f3134621183e453fa972e7c51317fec
zander312 <iskander93@gmail.com>
2017-01-31 20:43:42 -0500

    removes pii from db.js file

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 var knex = require('knex')({
   client: 'mysql',
   connection: {
-    host : 'sbaloadtime.clofitdcjrc6.us-east-1.rds.amazonaws.com',
-    port : '3306',
-    user : 'zander312',
-    password : 'incubus980',
-    database : 'loadtimes'
+    host : 'placeholder',
+    port : 'placeholder',
+    user : 'placeholder',
+    password : 'placeholder',
+    database : 'placholder'
   }
 });
 
